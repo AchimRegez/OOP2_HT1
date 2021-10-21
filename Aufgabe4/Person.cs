8a78b6204889e905b18ba941c689649465a4fb11
AchimRegez <51678296+AchimRegez@users.noreply.github.com>
2021-10-21 17:46:58 +0200

    Aufgabe 4 fertig

--- Aufgabe4/Person.cs
+++ Aufgabe4/Person.cs
@@ -6,27 +6,13 @@ using System.Threading.Tasks;
 
 namespace Aufgabe4
 {
-    public enum Gender { Male, Female}
+    public enum Gender { Male, Female }
     class Person
     {
-        Gender privateObj;
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string SecondName { get; set; }
         public int Age { get; set; }
-
-        //public Person(int id, string firstName, string secondName, int age)
-        //{
-        //    Id = id;
-        //    FirstName = firstName;
-        //    SecondName = secondName;
-        //    Age = age;
-        //}
-
-        public Person()
-        {
-        }
-
-        public Gender GenderProperty { get; set; }        
+        public Gender GenderProperty { get; set; }
     }
 }
--- Aufgabe4/Program.cs
+++ Aufgabe4/Program.cs
@@ -33,13 +33,30 @@ namespace Aufgabe4
 
                 new Person()
                 {
-                    Id = 5, FirstName = "Vanessa", SecondName = "Schuhmacher", Age = 50, GenderProperty = Gender.Female
+                    Id = 5, FirstName = "Vanessa", SecondName = "Schuhmacher", Age = 55, GenderProperty = Gender.Female
                 },
 
                 new Person()
                 {
-                    Id = 5, FirstName = "Reto", SecondName = "Schuhmacher", Age = 16, GenderProperty = Gender.Male
+                    Id = 6, FirstName = "Reto", SecondName = "Schuhmacher", Age = 62, GenderProperty = Gender.Male
+                },
+
+                new Person()
+                {
+                    Id = 7, FirstName = "Martin", SecondName = "Anderst", Age = 25, GenderProperty = Gender.Male
+                },
+
+                new Person()
+                {
+                    Id = 7, FirstName = "Felix", SecondName = "Baum", Age = 32, GenderProperty = Gender.Male
+                },
+
+                new Person()
+                {
+                    Id = 7, FirstName = "Martin", SecondName = "Maler", Age = 38, GenderProperty = Gender.Male
                 }
+
+
             };
 
             // LING Query
@@ -61,22 +78,45 @@ namespace Aufgabe4
 
             //Geben Sie die Anzahl Personen aus, welche zwischen 50 und 90 Jahren alt sind            
             var persons2 = (from p in _personsList
-                           where p.Age > 49 || p.Age < 91
+                           where p.Age > 49 && p.Age < 91
                            select p).Count();
 
 
             Console.WriteLine("Personen zwischen 50 und 90 Jahren:");
             Console.WriteLine(persons2);
+            Console.WriteLine();
 
+            ////Geben Sie alle weiblichen Personen aus, welche ein ‚e‘ im FirstName haben, sortiert nach aufsteigendem SecondName
+            var persons3 = from p in _personsList
+                           where p.GenderProperty == Gender.Female
+                           where p.FirstName.Contains("e")
+                           orderby p.SecondName ascending
+                           select p;
 
-            //Geben Sie alle weiblichen Personen aus, welche ein ‚e‘ im FirstName haben, sortiert nach aufsteigendem SecondName
-            var persons2 = from p in _personsList
-                           where (p.GenderProperty = Gender.Female) && (p.FirstName = "%e%")
-                           orderby p.SecondName;
+            Console.WriteLine("Alle weiblichen Personen, welche ein ‚e‘ im FirstName haben, sortiert nach aufsteigendem SecondName:");
+
+            foreach (var person in persons3)
+            {
+                Console.WriteLine("Vorname: {0} Nachname: {1} Alter: {2} Geschlecht: {3}", person.FirstName, person.SecondName, person.Age, person.GenderProperty);
+            }
+
+            Console.WriteLine();
 
 
             // Geben Sie alle männlichen Personen aus, welche zwischen 20 und 40 Jahren alt sind und deren Nachnahmen ein ‚a‘ enthält, sortiert nach absteigendem Alter
+            var persons4 = from p in _personsList
+                           where p.GenderProperty == Gender.Male
+                           where p.Age > 19 && p.Age < 41
+                           where p.SecondName.Contains("a")
+                           orderby p.Age descending
+                           select p;
 
+            Console.WriteLine("Alle männlichen Personen, welche zwischen 20 und 40 Jahren alt sind und deren Nachnahmen ein ‚a‘ enthält, sortiert nach absteigendem Alter:");
+
+            foreach (var person in persons4)
+            {
+                Console.WriteLine("Vorname: {0} Nachname: {1} Alter: {2} Geschlecht: {3}", person.FirstName, person.SecondName, person.Age, person.GenderProperty);
+            }
 
             Console.ReadKey();
         }        
